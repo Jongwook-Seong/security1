c9a62595d7f3ff879893fa5e57f948eb9dd1a1c7
Xavier Jongwook Seong <ypwa121@gmail.com>
2024-03-14 16:03:33 +0900

    learn: 구글 로그인 준비

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인을 진행한다.
                 .defaultSuccessUrl("/"));
+        http.oauth2Login(form -> form.loginPage("/loginForm"));
         return http.build();
     }
 }
